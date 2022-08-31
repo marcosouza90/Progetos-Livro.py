3baab00fa22da8563cd0abe397f6dc4d894b9752
marcos souza <marcosouza90>
2022-08-31 08:33:22 -0300

    commit inicial

---
+++ Livro.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um numero: "))
+if numero == 1:
+    print("Um")
+elif numero == 2:
+    print("Dois")
+elif numero == 3: 
+    print("Tres")
+else:
+    print("nao sei")       
+
